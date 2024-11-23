578194d8b0369d8aede6b51753004131fb264592
Md Safiqul Islam Nasim <mdsafiqulislamnasim1234@gmail.com>
2024-11-23 16:53:14 +0600

    Second Part

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,60 @@
+<?php
+
+// app/Http/Controllers/ProductController.php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+         $products = Product::all();
+         return view('backend.layout.products.index', compact('products'));;
+    }
+
+    public function create()
+    {
+        return view('backend.layout.products.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([    
+            'name' => 'required|string|max:255',
+            'price' => 'required|numeric',
+            'stock' => 'required|integer',
+        ]);
+
+        Product::create($request->all());
+
+        return redirect()->route('products.index')->with('success', 'Product added successfully');
+    }
+
+    public function edit(Product $product)
+    {
+        return view('backend.layout.products.edit', compact('product'));
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'price' => 'required|numeric',
+            'stock' => 'required|integer',
+        ]);
+
+        $product->update($request->all());
+
+        return redirect()->route('products.index')->with('success', 'Product updated successfully');
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return redirect()->route('products.index')->with('success', 'Product deleted successfully');
+    }
+}
